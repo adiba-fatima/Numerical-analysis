0c8e8e7ef2ffcf5a7127cce9c35114d3904fed01
Adiba Fatima <adibafatima1006@gmail.com>
2026-04-06 20:55:53 +0600

    Add files via upload

---
+++ Numerical Analysis/fixedpoint.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double g(double x)
+{
+    return (cos(x)+1)/3;
+}
+int main()
+{
+    double x0=0;
+    double x1;
+    int i;
+    cout<<"Iteration\tRoot Approximation\n";
+    for(i=1; i<=5;i++)
+    {
+        x1=g(x0);
+        cout<<i<<"\t\t"<<x1<< endl;
+        x0=x1;
+    }
+    cout<<"Approximate root= "<<x1;
+}
